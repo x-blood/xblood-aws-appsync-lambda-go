4df6b6387d82505a430022eb2e3cd7589aa03e51
x-blood <yasaisato@gmail.com>
2019-02-08 17:18:13 +0900

    [add] Added deploy files

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Deploy
+go build ./src/handlers/handler.go
+
+# Create Package
+aws cloudformation package \
+  --template-file template.yml \
+  --output-template-file template-output.yml \
+  --s3-bucket handson20180323-sam-packages \
+  --profile handson20180323
+
+# Deploy
+aws cloudformation deploy \
+  --template-file template-output.yml \
+  --stack-name appsync-lambda-go \
+  --capabilities CAPABILITY_IAM \
+  --profile handson20180323
--- src/handlers/handler.go
+++ src/handlers/handler.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-func HandleRequest(ctx context.Context) (string, error) {
-	return fmt.Sprintf("SandBox!Test!"), nil
+type Post struct {
+	Pk string `json:"pk"`
+	Sk string `json:"sk"`
+}
+
+func hander(arg Post) (Post, error) {
+
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"pk": {
+				S: aws.String(arg.Pk),
+			},
+			"sk": {
+				S: aws.String(arg.Sk),
+			},
+		},
+		TableName: aws.String("post"),
+	}
+
+	session, err := session.NewSession(
+		&aws.Config{Region: aws.String("ap-northeast-1")},
+	)
+
+	if err != nil {
+		return Post{}, err
+	}
+
+	svc := dynamodb.New(session)
+
+	_, err = svc.PutItem(input)
+
+	return arg, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(hander)
 }
